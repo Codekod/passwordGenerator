426194afb2666bd9052559427770690dc78c4d0e
Codekod <meliheken@aof.anadolu.edu.tr>
2023-04-01 01:10:43 +0300

    New files uplod

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+    "/"];
 
 let passwordLength = 12
-let input1El = document.getElementById("input1")
-let input2El = document.getElementById("input2")
+let password1 = document.getElementById("password1")
+let password2 = document.getElementById("password2")
 
 function getRandomCharacters() {
     let randomChar = Math.floor(Math.random() * characters.length)
@@ -18,20 +17,11 @@ function generateRandomPassword() {
     }
     return randomPassword
 }
+function buttonAs() {
 
-
-
-
-
-
-function buttonas() {
-
-    input1El.value=generateRandomPassword()
-    input2El.value=generateRandomPassword()
+    password1.innerText=generateRandomPassword()
+    password2.innerText=generateRandomPassword()
 }
-
-
-
 
 
 
